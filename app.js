6354b24b1ad503ebc41491be7da4158f266b5f22
AlexanderAntov <a.antov91@gmail.com>
2016-12-05 20:24:44 +0200

    Commit leftover changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 ﻿var express = require('express'),
     schedule = require('node-schedule'),
-    http = require('http'),
-    httpServices = require('./http-services.js')(),
+    cacheService = require('./cache-service.js')(),
     app = express(),
     dataCache = {
         news: null,
@@ -30,52 +29,18 @@ app.get('/reset-cache', function (req, res) {
     res.send(true);
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+app.set('port', process.env.PORT || 8080);
 
-//heroku
-if (process.env.PORT) {
-    app.listen(app.get('port'), function() {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
-} else { //openshift
-    http.createServer(app).listen(app.get('port'), app.get('ip'), function () {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
-}
-
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
 function setUpSchedule() {
-    schedule.scheduleJob({ hour: 6 }, setUpCache);
+    schedule.scheduleJob({ hour: 7 }, setUpCache);
     schedule.scheduleJob({ hour: 17 }, setUpCache);
 }
 
 function setUpCache() {
-    cacheNews();
-    //cacheWeather();
-
-    function cacheNews() {
-        var newsDataPromises = [
-            httpServices.newYorkTimes(),
-            httpServices.theGuardian()
-        ];
-
-        Promise.all(newsDataPromises).then(function (dataModelLists) {
-            var newsModelsList = [];
-            dataModelLists.forEach(function (dataModelList) {
-                newsModelsList = newsModelsList.concat(dataModelList);
-            });
-            dataCache.news = newsModelsList;
-        });
-    }
-
-    function cacheWeather() {
-        httpServices.weatherData('Sofia').then(function (dataModelLists) {
-            var weatherModelsList = [];
-            dataModelLists.forEach(function (dataModelList) {
-                weatherModelsList = weatherModelsList.concat(dataModelList);
-            });
-            dataCache.weather = weatherModelsList;
-        });
-    }
+    cacheService.news(dataCache);
+    //cacheService.weather(dataCache, 'Sofia');
 }
---
+++ cache-service.js
@@ -0,0 +1,29 @@
+﻿module.exports = function () {
+    var httpService = require('./http-service.js')();
+
+    return {
+        news: function (cache) {
+            var newsDataPromises = [
+                httpService.newYorkTimes(),
+                httpService.theGuardian()
+            ];
+
+            Promise.all(newsDataPromises).then(function (dataModelLists) {
+                var newsModelsList = [];
+                dataModelLists.forEach(function (dataModelList) {
+                    newsModelsList = newsModelsList.concat(dataModelList);
+                });
+                cache.news = newsModelsList;
+            });
+        },
+        weather: function (cache, city) {
+            httpService.weatherData(city).then(function (dataModelLists) {
+                var weatherModelsList = [];
+                dataModelLists.forEach(function (dataModelList) {
+                    weatherModelsList = weatherModelsList.concat(dataModelList);
+                });
+                cache.weather = weatherModelsList;
+            });
+        }
+    };
+};
