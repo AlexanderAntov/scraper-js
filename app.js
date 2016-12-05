6984e488003480ba6765562dbc8140c1b61b3017
AlexanderAntov <a.antov91@gmail.com>
2016-12-05 16:42:36 +0200

    Added an if block for heroku

--- app.js
+++ app.js
@@ -30,12 +30,20 @@ app.get('/reset-cache', function (req, res) {
     res.send(true);
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-http.createServer(app).listen(app.get('port'), app.get('ip'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+//heroku
+if (process.env.PORT) {
+    app.listen(app.get('port'), function() {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+} else { //openshift
+    http.createServer(app).listen(app.get('port'), app.get('ip'), function () {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+}
+
 
 function setUpSchedule() {
     schedule.scheduleJob({ hour: 6 }, setUpCache);
