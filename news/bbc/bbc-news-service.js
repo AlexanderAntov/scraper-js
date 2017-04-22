8128027a7cbebf85d81fbf1c48815b84ec7fc8cc
AlexanderAntov <a.antov91@gmail.com>
2017-04-22 20:10:48 +0300

    Remove extra wrapper

--- news/bbc/bbc-news-service.js
+++ news/bbc/bbc-news-service.js
@@ -12,17 +12,15 @@
             function dataTransformer(data) {
                 var articlesArray = [];
                 parseXMLString(data, function (err, result) {
-                    parseXMLString(data, function (err, result) {
-                        result.rss.channel[0].item.forEach(function (newsItemData) {
-                            articlesArray.push(newsModelFactory.get({
-                                title: newsItemData.title[0],
-                                info: httpService.trim(newsItemData.description ? newsItemData.description[0] : ''),
-                                url: newsItemData.link[0],
-                                image: newsItemData['media:thumbnail'] ? newsItemData['media:thumbnail'][0]['$'].url : null,
-                                dateTime: newsItemData.pubDate ? newsItemData.pubDate[0] : '',
-                                provider: 'bbc'
-                            }));
-                        });
+                    result.rss.channel[0].item.forEach(function (newsItemData) {
+                        articlesArray.push(newsModelFactory.get({
+                            title: newsItemData.title[0],
+                            info: httpService.trim(newsItemData.description ? newsItemData.description[0] : ''),
+                            url: newsItemData.link[0],
+                            image: newsItemData['media:thumbnail'] ? newsItemData['media:thumbnail'][0]['$'].url : null,
+                            dateTime: newsItemData.pubDate ? newsItemData.pubDate[0] : '',
+                            provider: 'bbc'
+                        }));
                     });
                 });
                 return articlesArray;
