c420d602c2d283e46a9799958ec94a6e54320236
AlexanderAntov <a.antov91@gmail.com>
2017-04-30 21:10:44 +0300

    Info text fixes

--- news/tech-and-science/sciencemag/sciencemag-news-service.js
+++ news/tech-and-science/sciencemag/sciencemag-news-service.js
@@ -10,18 +10,30 @@
             return httpService.performGetRequest(options, dataTransformer);
 
             function dataTransformer(data) {
-                var articlesArray = [];
+                var articlesArray = [],
+                    currentModel = null,
+                    currentInfo = null;
 
                 parseXMLString(data, function (err, result) {
                     result['rdf:RDF'].item.forEach(function (newsItemData) {
-                        articlesArray.push(newsModelFactory.get({
+                        currentInfo = httpService.trim(
+                            newsItemData
+                                .description[0]
+                                .replace(/<(?:.|\n)*?>/gm, '')
+                                .replace(/\n/, '')
+                        );
+                        currentModel = newsModelFactory.get({
                             title: newsItemData.title[0],
-                            info: httpService.trim(newsItemData.description[0].replace(/<(?:.|\n)*?>/gm, '')).replace(/\n/, ''),
+                            info: currentInfo,
                             url: newsItemData.link[0],
                             image: null,
                             dateTime: newsItemData['dc:date'][0],
                             provider: 'sciencemag'
-                        }));
+                        });
+
+                        if (currentModel.info) {
+                            articlesArray.push(currentModel);
+                        }
                     });
                 });
                 return articlesArray;
--- news/tech-and-science/techcrunch/techcrunch-news-service.js
+++ news/tech-and-science/techcrunch/techcrunch-news-service.js
@@ -1,5 +1,6 @@
 ﻿module.exports = function () {
-    var parseXMLString = require('xml2js').parseString,
+    var cheerio = require('cheerio'),
+        parseXMLString = require('xml2js').parseString,
         apiConstants = require('../../../common/api-constants.js')(),
         httpService = require('../../../common/http-service.js')(),
         newsModelFactory = require('../../../common/news-model-factory.js')();
@@ -10,13 +11,22 @@
             return httpService.performGetRequest(options, dataTransformer);
 
             function dataTransformer(data) {
-                var articlesArray = [];
+                var articlesArray = [],
+                    currentInfo = null;
 
                 parseXMLString(data, function (err, result) {
                     result.rss.channel[0].item.forEach(function (newsItemData) {
+                        currentInfo = cheerio.load(
+                            httpService.trim(
+                                newsItemData
+                                .description[0]
+                                .replace(/<(?:.|\n)*?>/gm, '')
+                                .replace(/&nbsp;/, '')
+                            )
+                        ).text();
                         articlesArray.push(newsModelFactory.get({
                             title: newsItemData.title[0],
-                            info: httpService.trim(newsItemData.description[0].replace(/<(?:.|\n)*?>/gm, '')).replace(/&nbsp;/, ''),
+                            info: currentInfo,
                             url: newsItemData.link[0],
                             image: null,
                             dateTime: newsItemData.pubDate[0],
